5deb42362b289eced93fab0d4045a95d558252a0
takumisamurai <bluemonetdev@gmail.com>
2025-09-13 20:51:00 +0530

    package name updated

--- src/test/java/com/javatech/configserver/SpringCloudConfigServerApplicationTests.java
+++ src/test/java/com/javatech/configserver/SpringCloudConfigServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.javatech.spring_cloud_config_server;
+package com.javatech.configserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
